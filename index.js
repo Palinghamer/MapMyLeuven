79146d3020cd6511fc6fd3a3e0594b1ff12c7c58
bauke <bauke.verschueren@gmail.com>
2025-03-25 12:18:31 +0100

    trying to connect event data

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const eventRoutes = require("node_modules/models/event.js");
+const eventRoutes = require("./routes/events");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("node_modules/models/event.js", eventRoutes);
+app.use("./node_modules/routes/events", eventRoutes);
 
 mongoose.connect(process.env.MONGO_URI, {
-}).then(() => console.log("Connected to MongoDB"))
+    // options if needed
+})
+    .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error(err));
 
 app.listen(3000, () => console.log("Server running on port 3000"));
--- routes/events.js
+++ routes/events.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Event = require("../models/Event");
+const Event = require("../models/event.js");
 
 const router = express.Router();
 
