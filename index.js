687464295acfa5bf264658ffcbaf4017e421180d
bauke <bauke.verschueren@gmail.com>
2025-04-10 22:24:42 +0200

    getting mongodb to work for everyone4

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
 })
     .then(() => console.log("✅ Connected to MongoDB Atlas"))
     .catch(err => console.error("❌ MongoDB connection error:", err));
