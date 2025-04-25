48e9267ff742caacc04290d547ee11fdc5004512
giulianuvoli <giulia.nuvoli99@gmail.com>
2025-04-25 13:55:35 +0200

    Current weather API, with F degrees function

--- weathercode.js
+++ weathercode.js
@@ -7,6 +7,11 @@ function loadWeatherData(){
     const appid = "4208f98548c8c95a759522b1c4c76230"
     const url="https://api.openweathermap.org/data/2.5/weather?q=Leuven&units=metric&appid=4208f98548c8c95a759522b1c4c76230";
 
+    //function to convert celsius degrees into fahrenheit degrees
+    function celsiusToFahrenheit(celsius){
+        return (celsius * 9/5) + 32;
+    }
+
     fetch(url)
         .then((response) => response.json())
         .then (function(data){
@@ -15,7 +20,7 @@ function loadWeatherData(){
 
 
             output += "Current weather: " + data.weather[0].main+ ". ";
-            output += "The current temperature is " +data.main.temp+ " degrees. ";
+            output += "The current temperature is " +data.main.temp+ " °C (" + celsiusToFahrenheit(data.main.temp).toFixed(1) + " °F).";
 
             output += "<br><br>MapMyLeauven recommends: "
 
@@ -23,6 +28,10 @@ function loadWeatherData(){
             if (data.weather[0].main === 'Clouds' || data.weather[0].main === 'Rain' || data.weather[0].main === 'Snow'){
 
                 output += "Do not forget your umbrella! "
+            }
+
+            if (data.weather[0].main === 'Mist') {
+                output += "There’s a chance of rain, better bring an umbrella. "
             }
 
             if (data.weather[0].main === 'Clear' || data.weather[0].main === 'Sun') {
@@ -41,7 +50,7 @@ function loadWeatherData(){
             }
 
             if (data.main.temp >= 5 && data.main.temp <= 14) {
-                output += "Wear a jacket. "
+                output += "Wear a jacket! It is going to be chilly. "
             }
 
             if (data.main.temp < 5) {
